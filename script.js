2a3f9c0724cd8e9d1963b2e785af00cf1e571014
freeseodotbiz <freeseodotbiz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-12 17:05:38 +0000

    added the ability to have certain countries show up at the top of list
    git-svn-id: https://plugins.svn.wordpress.org/geographic-selects/trunk@384110 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ jQuery(document).ready(function($) {
 
  $("#geographicSelectsOptions :input").live('click',function(e){
   e.preventDefault();
+  if ($(this).attr("name")=="addCountryToOptGroup") return;
   var currentForm=$(this).parents("form").attr("name");
   var params={};
 
-  $("#"+currentForm+" :input").each(function(index,value){
+  $("#"+currentForm+" :input").not(":input[type=radio][checked=false]").not(":input[type=checkbox][checked=false]").each(function(index,value){
    params[$(value).attr("name")]=$(value).val();
   });
   
+  params["formName"]=currentForm;
+  
   imageMarginLeft=Math.round((parseInt($("#"+currentForm+"_div").css('width'))-220)/2);
-  $(sectionToReplace).html('<p style=\"color: #5F818A; font-size: 25px; text-align: center; padding-top: '+showStandby_marginTop+'px;\">Standby...</p>');
+  $("#"+currentForm+"_div").html('<p style=\"color: #5F818A; font-size: 25px; text-align: center; padding-top: 25px;\">Standby...</p>');
 
   params['action']='geographicSelects_AJAX_handler';	
 
-  $(sectionToReplace).load(adminAjaxDir,params, function(response, status, xhr) {
-   if (status=="error") $(sectionToReplace).html('Sorry, your browser is having trouble communicating with our server at this time.');
+  $("#"+currentForm+"_div").load(adminAjaxDir,params, function(response, status, xhr) {
+   if (status=="error") $("#"+currentForm+"_div").html('Sorry, your browser is having trouble communicating with our server at this time.');
   });
 
  });
